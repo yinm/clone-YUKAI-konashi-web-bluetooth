1d0a06c3c50ad65fbfc555b50d1590514a5af2d0
yinm <yinm.cminor9@gmail.com>
2018-06-03 13:22:06 +0900

    Add INPUT & OUTPUT

--- test/tests.js
+++ test/tests.js
@@ -28,5 +28,16 @@ Konashi.find(true).then(k => {
         })
     })
 
-  })
+    QUnit.test('INPUT & OUTPUT', assert => {
+      return k.pinMode(k.PIO0, k.INPUT)
+        .then(() => k.pinMode(k.PIO1, k.OUTPUT))
+        .then(() => k.pinMode(k.PIO2, k.INPUT))
+        .then(() => k.pinMode(k.PIO3, k.OUTPUT))
+        .then(() => k.pinMode(k.PIO4, k.INPUT))
+        .then(() => {
+          assert.ok(true)
+        })
+    })
+  }) // .pinMode
+
 })
