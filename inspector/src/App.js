034c97398327b59beaccd85d37c26de4fa7d4277
yinm <yinm.cminor9@gmail.com>
2018-06-10 14:49:37 +0900

    create-react-app

---
+++ inspector/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+      </div>
+    );
+  }
+}
+
+export default App;
